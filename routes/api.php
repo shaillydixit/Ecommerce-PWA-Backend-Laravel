957d04674b383835e96df548e65e8197875ddc5f
SD <IamCodeX@Shaillys-MacBook-Air.local>
2022-01-08 05:47:33 +0530

    site info api created

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\SiteInfoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
+Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
